5c3ca17e12b089d2024b1d219235cc879a2ef251
Mono <wz9384@gmail.com>
2024-12-02 03:59:06 +0900

    perplexity 주석 변경

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from utils import calc_perplexity
 
 app = Flask(__name__)
@@ -13,9 +13,20 @@ def t_sne():
 
 @app.route('/perplexity')
 def perplexity():
+
+    # request를 통해 증강 종류 확인
+    augment_type = request.args.get()
+
+    # 증강 종류에 따라 model_id 지정
+    model_type = ""
     data = "static/data/sample.csv" # perplexity 계산을 위한 데이터 셋 경로
-    model_origin = "models/origin_model" # origin 모델 경로
-    model_aug = "models/noise_model" # aug 모델 경로
+
+    # 모델 경로 설정
+    model_path = "models/"
+    model_origin = model_path + "origin_model" # origin 모델 경로
+    model_aug = model_path + model_type # aug 모델 경로
+
+    
     perplexity_origin = calc_perplexity(model_origin,data)
     perplexity_augmented = calc_perplexity(model_aug,data)
 
